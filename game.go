e6f853b50e820ac67c905e9e6119145e8f572716
scarfacedotcom <peteradaaku@gmail.com>
2023-06-27 12:16:08 +0100

    first commit

---
+++ game.go
@@ -0,0 +1,24 @@
+package main
+
+//--Summary:
+//  Implement receiver functions to create stat modifications
+//  for a video game character.
+//
+//--Requirements:
+//* Implement a player having the following statistics:
+//  - Health, Max Health
+//  - Energy, Max Energy
+//  - Name
+type player struct {
+	name              string
+	health, maxHealth uint
+	energy, maxEnergy uint
+}
+
+//* Implement receiver functions to modify the `Health` and `Energy`
+//  statistics of the player.
+//  - Print out the statistic change within each function
+//  - Execute each function at least once
+func main() {
+
+}
