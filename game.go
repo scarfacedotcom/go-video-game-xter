e59cd716da2cf1dc7b9dfc45b7ce55f19374d6e9
scarfacedotcom <peteradaaku@gmail.com>
2023-06-27 12:31:16 +0100

    add health

--- game.go
+++ game.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //--Summary:
 //  Implement receiver functions to create stat modifications
 //  for a video game character.
@@ -17,6 +19,15 @@ type player struct {
 
 //* Implement receiver functions to modify the `Health` and `Energy`
 //  statistics of the player.
+func (player *Player) addHealth(amount uint) {
+	player.health += amount
+	if player.health > player.maxHealth {
+		player.health = player.maxHealth
+	}
+	fmt.Println(player.name, "Add", amount, "health ->", player.health)
+
+}
+
 //  - Print out the statistic change within each function
 //  - Execute each function at least once
 func main() {
